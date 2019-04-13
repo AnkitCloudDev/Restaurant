47cb73c2a599a099283cf2936c5578e0f9eb2683
Ankit Dongre <x18147054@student.ncirl.ie>
2019-04-13 12:49:16 +0000

    Added tinfoild

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-config.ssl_options = {  redirect: { status: 301, port: 81 } }
-config.ssl_options = { hsts: { preload: true } }
+config.ssl_options = {  redirect: { status: 301, port: 81 } , hsts: { expires: 10.days } }
+
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = :debug
