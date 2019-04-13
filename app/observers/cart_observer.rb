186e05223d9f182fac0927aef9ac3dbe3cc1e611
Ankit Dongre <x18147054@student.ncirl.ie>
2019-04-13 15:01:40 +0000

    Commented Observer

--- app/observers/cart_observer.rb
+++ app/observers/cart_observer.rb
@@ -2,11 +2,12 @@ class CartObserver < ActiveRecord::Observer
  observe :order
  
   def after_update(order)
- puts("observer called----------------<><><><><>?<<><><><><><><>--------------")  
- @user=User.find(current_user.id)
- if(order.previous_changes.any?)
-	  ApplicationMailer.send_observer_mail(@user).deliver
-		  
- end
+   
+# puts("observer called----------------<><><><><>?<<><><><><><><>--------------")  
+# @user=User.find(current_user.id)
+# if(order.previous_changes.any?)
+#  ApplicationMailer.send_observer_mail(@user).deliver
+#	  
   end
+ # end
 end
