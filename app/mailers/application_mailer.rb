e2a5b438988a2a1053ff2f0a1df04d4b3127f839
Ankit Dongre <x18147054@student.ncirl.ie>
2019-04-14 22:11:40 +0000

    added Profile observer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,14 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'charlienoreply@gmail.com'
   layout 'mailer'
+  #send mail after order is placed
   def send_cart_mail(user)
       
        mail to: user.email, subject: "Cart", from:'charlienoreply@gmail.com'
   end
+   #send mail after profile is updated
   def send_observer_mail(user)
        mail to: user.email, subject: "Changes to Cart", from:'charlienoreply@gmail.com'
   end
-    def send_new_product(user)
-       mail to: user.email, subject: "Changes to Cart", from:'charlienoreply@gmail.com'
-    end
+   
 end
